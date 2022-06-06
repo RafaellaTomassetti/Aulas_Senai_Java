3dc5225c86900f4f76e994685b2d0fedb4e36abe
RafaellaTomassetti <102962698+RafaellaTomassetti@users.noreply.github.com>
2022-06-06 09:01:07 -0300

    Update Aplicacao.java
    verdade

--- ClasseAbstrata/src/Aplicacao.java
+++ ClasseAbstrata/src/Aplicacao.java
@@ -5,7 +5,7 @@ public class Aplicacao {
 		
 		//Item item = new Item();
 		
-		Item item = new Moeda(); //Quando vocÍ cria a vari·vel, n„o È nescess·rio criar novamente com o "Item item", apenas coloque a vari·vel e conecte com o mÈtodo
+		Item item = new Moeda(); //Quando voc√™ cria a vari√°vel, n√£o √© nescess√°rio criar novamente com o "Item item", apenas coloque a vari√°vel e conecte com o m√©todo
 			item.pegar();
 			
 			item = new Cogumelo();
@@ -15,7 +15,7 @@ public class Aplicacao {
 			item.pegar();
 		
 
-	}
+	} //A Rafa √© ridicula
 
 	
 	
